fdbda0b603cd188d6332cc0b8cf3877a8691a331
aujaja35-cmd <aujaja35@gmail.com>
2025-11-25 07:41:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/-Group-9-Task-Manager-/',  // added for GitHub Pages
   server: {
     host: "::",
     port: 8080,
