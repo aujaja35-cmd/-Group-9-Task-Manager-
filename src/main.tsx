ed2b40ae4084b6f6711ec13ae690848f8328f713
aujaja35-cmd <aujaja35@gmail.com>
2025-11-25 07:15:22 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-// Wrap App in BrowserRouter with basename for GitHub Pages
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/-Group-9-Task-Manager-">
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
