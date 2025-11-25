def920f2748a8e4ed283c33c6697c041b3afc3b2
aujaja35-cmd <aujaja35@gmail.com>
2025-11-25 06:40:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/-Group-9-Task-Manager-/",
+  base: "/-Group-9-Task-Manager-/", // <--- Add this line
   server: {
     host: "::",
-    port: 8080
+    port: 8080,
   },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
